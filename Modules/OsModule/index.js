41818a8b16ff4bdf2f63cb8c6ce10fd9f68ed005
Siddharth6511 <vashishthatushar004@gmail.com>
2025-08-07 15:42:31 +0530

    path module

--- Modules/OsModule/index.js
+++ Modules/OsModule/index.js
@@ -1,4 +1,13 @@
-const os = require('os');
-console.log(os.type());
-console.log(os.version())
-console.log(os.userInfo())
+// const os = require('os');
+// console.log(os.type());
+// console.log(os.version())
+// console.log(os.userInfo())
+
+
+
+//path module
+
+const path = require('path');
+console.log(path.dirname(__dirname));
+console.log(path.parse(__dirname));
+console.log(path.extname(__dirname));
