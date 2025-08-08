2d6cd8f0a636f42ceb0b34ddbc3adba28d263dd3
Siddharth6511 <vashishthatushar004@gmail.com>
2025-08-08 19:13:26 +0530

    readung file using promises

--- Modules/FS/index.js
+++ Modules/FS/index.js
@@ -1,28 +1,38 @@
-const fs = require('fs');
-fs.readFile('Modules/FS/file1.txt', 'utf-8', (err, data)=> {
-    if(err){
-        console.log(err.message)
-        return;
-    }
-    console.log(data);
-    if(data){
-        fs.readFile('Modules/FS/file2.txt', 'utf-8', (err,data)=> {
-            if(err){
-                console.log(err.message)
-                return;
-            }
-            console.log(data)
-            if(data){
-                fs.readFile('Modules/FS/file3.txt', 'utf8', (err, data)=> {
-                    if(err){
-                        console.log(err.message)
-                        return;
-                    }
-                    else{
-                        console.log(data)
-                    }
-                })
-            }
-        })
-    }
-})
+// const fs = require('fs');
+// fs.readFile('Modules/FS/file1.txt', 'utf-8', (err, data)=> {
+//     if(err){
+//         console.log(err.message)
+//         return;
+//     }
+//     console.log(data);
+//     if(data){
+//         fs.readFile('Modules/FS/file2.txt', 'utf-8', (err,data)=> {
+//             if(err){
+//                 console.log(err.message)
+//                 return;
+//             }
+//             console.log(data)
+//             if(data){
+//                 fs.readFile('Modules/FS/file3.txt', 'utf8', (err, data)=> {
+//                     if(err){
+//                         console.log(err.message)
+//                         return;
+//                     }
+//                     else{
+//                         console.log(data)
+//                     }
+//                 })
+//             }
+//         })
+//     }
+// })
+
+
+const fsPromise = require('fs').promises;
+
+const readFile = async () => {
+    const data = await fsPromise.readFile('Modules/FS/file1.txt', 'utf-8');
+    console.log(data)
+}
+
+readFile()
